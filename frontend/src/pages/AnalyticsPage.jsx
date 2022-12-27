d6d7e6aac5e68148aceb560c91c2c79aebf1c6e9
CamillaMazzoleni <109732478+CamillaMazzoleni@users.noreply.github.com>
2022-12-27 10:43:25 +0100

    Analytics Page and settings (#10)
    * Analytics page
    
    charts and widget
    
    * Deleted bug
    
    Analytics page updated
    
    * Analytics page + settingspage
    
    Fixed menu in the corner + settingspage

---
+++ frontend/src/pages/AnalyticsPage.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./AnalyticsPage.css";
+import Widget from "../components/Widget";
+import Chart from "../components/Chart";
+
+const assets=[
+    {
+      amount: '39,354',
+      percentage: '-4',
+      title: 'Apple',
+    
+    },
+    {
+      amount: '4,396',
+      percentage: '+23',
+      title: 'Tesla',
+      
+    },
+    {
+      amount: '423,39',
+      percentage: '+38',
+      title: 'Microsoft',
+      
+    },
+    {
+      amount: '39,354',
+      percentage: '-12',
+      title: 'Eni',
+     
+    },
+  ];
+
+class AnalyticsPage extends React.Component{
+    constructor(props){
+      super(props);
+    }
+    render(){
+        return (
+         
+        <div className="home">
+           <div className="menu"> 
+            <button onClick={this.props.apriMenu} className='menu'><i className='fas fa-bars'/></button>
+          </div>
+          
+            <div className="homeContainer">
+
+              <div className="charts">
+                <Chart title="Investment growth" aspect={2 / 1} />
+              </div>
+              <div className="widgets">
+                {assets.map((item)=>(
+                  <Widget prop={item}/>
+                ))}
+              </div>
+            
+            </div>
+        </div>);
+    }
+}
+
+export default AnalyticsPage; 
