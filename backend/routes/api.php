141100040dc166e1f356ae8c707cbc83baf21646
Andrea Rota <andrea.rota.98@gmail.com>
2023-01-03 16:18:38 +0100

    Add route only for admin

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,11 +18,24 @@ use Illuminate\Support\Facades\Route;
 /**
  * API to handle the core logic of the authentication process.
  */
-Route::controller(UserAdmin::class)->prefix('auth')->group(function () {
+Route::controller(UserBase::class)->prefix('auth')->group(function () {
     Route::post('login', 'login')->name('login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::get('stocks', 'getStocks');
+});
 
+/**
+ * API to handle stocks.
+ */
+Route::controller(UserBase::class)->prefix('stocks')->group(function () {
+    Route::get('', 'getStocks');
+});
+
+/**
+ * API to handle stocks.
+ */
+Route::controller(UserAdmin::class)->prefix('admin/stocks')->middleware('onlyAdmin')->group(function () {
+    Route::get('', 'getStocks');
 });
