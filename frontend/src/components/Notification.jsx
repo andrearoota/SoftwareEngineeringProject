dce04a88d67e7d216270d549bc2ea003a4b65a11
CamillaMazzoleni <mazzolenicamilla001@gmail.com>
2023-01-04 00:09:45 +0100

    Notifications page
    notification page and components

---
+++ frontend/src/components/Notification.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import { GiPayMoney } from "react-icons/gi";
+import './Notification.css'
+function Notification({prop}) {
+    let data;
+    
+
+    switch(prop.type){
+        case "buy":
+            data = {
+                title: "BUY",
+                body: "Buying "
+            };
+            break;
+        case "sell":
+            data = {
+                title: "SELL",
+                body: "Selling "
+            };
+            break;
+    }
+  return (
+    <div className='notification'>
+        <GiPayMoney size={70} /> 
+        <div className='center'>
+        <Card border="dark">
+        <Card.Header>{data.title}</Card.Header>
+        <Card.Body>
+          <Card.Title>{prop.stock_name}</Card.Title>
+          <Card.Text>
+           {data.body} price per action is {prop.price}
+          </Card.Text>
+        </Card.Body>
+        </Card>
+        </div>
+    </div>
+  )
+}
+
+export default Notification;
