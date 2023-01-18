a4225e460ce15a35544e78eeb2d89e76cf9efe6c
pierGit7 <sorgiovanni60@gmail.com>
2023-01-18 11:33:36 +0100

    Update EOD.R

--- backend/script/EOD.R
+++ backend/script/EOD.R
@@ -35,10 +35,11 @@ library(jsonlite)
 library(dplyr)
 #this library is used to create SMA model
 library(smooth)
+
 #initializing  stock label
 symbols= c('AAPL','ABBV','ABT','ACN','AGN','AIG','ALL','AMGN','AMZN','AXP','BA','BAC','BIIB','BK','BLK','BMY','C','CAT','CELG','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DHR','DIS','DOW','DUK','EMC','EMR','EXC','F','FB','FDX','FOX','FOXA','GD','GE','GILD','GM','GOOG','GOOGL','GS','HAL','HD','HON','IBM','INTC','JNJ','JPM','KMI','KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MON','MRK','MS','MSFT','NEE','NKE','ORCL','OXY','PEP','PFE','PG','PM','PYPL','QCOM','RTN','SBUX','SLB','SO','SPG','T','TGT','TXN','UNH','UNP','UPS','USB','USD','UTX','V','VZ','WBA','WFC'
           
-)
+          )
 
 #initializing three different data_frame
 dati_totali<-data.frame(1:14)
@@ -50,17 +51,22 @@ for(s in symbols){
 
 
   r<- GET(paste('https://api.marketstack.com/v1/tickers/', s, '/eod', sep=''),add_headers(Name = 'access_key',Name='limit'),query=list(access_key='b7f513ecf8b91aa4dc345df69bf55a3d',limit=14))
+
+  #convert JSON text in dataframe
   dati<-fromJSON(content(r,as="text"))
   dati_totali[s]=rev(dati[["data"]][["eod"]][["adj_close"]])
+
   #sma model with two different time window for each stock
   SMA_3[s]<-sma(ts(dati_totali[s]),order = 3,silent = FALSE)$fitted
   SMA_7[s]<-sma(ts(dati_totali[s]),order = 7,silent = FALSE)$fitted
 }
+
 #binding date information to each value
 date<-rev(dati[["data"]][["eod"]][["date"]])
 SMA_3<-dplyr::bind_cols(date,SMA_3)
 SMA_7<-dplyr::bind_cols(date,SMA_7)
 
+#creating a list of buy_sel where each stock are checked 
 list_buy_sell<-buy_sell(SMA_3,SMA_7,dati_totali)
 
 
