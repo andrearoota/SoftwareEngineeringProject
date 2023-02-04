8b8297abec7f5a674893732a57f02eec1e862a0d
Andrea Rota <andrea.rota.98@gmail.com>
2023-02-04 13:34:38 +0100

    Modificata la generalizzazione

--- backend/app/Http/Controllers/User/UserAdmin.php
+++ backend/app/Http/Controllers/User/UserAdmin.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class UserAdmin extends UserAbstract
+class UserAdmin extends UserBase
 {
 
     /**
