5b8de0283748282baabde132702eaa0408521f2f
Andrea Rota <andrea.rota.98@gmail.com>
2023-02-07 20:30:19 +0100

    Format date

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -15,8 +15,8 @@ class SettingsPage extends React.Component {
       first_name: "a",
       last_name: "",
       gender: "",
-      birthdate:"",
-      cf:"",
+      birthdate: "",
+      cf: "",
       is_admin: false,
     }
   }
@@ -37,135 +37,137 @@ class SettingsPage extends React.Component {
       .then(response => response.json())
       .catch(error => console.log('error', error))
 
+    let formatbirthdate = new Date(resp.user.birthdate);
+
     this.setState({
       first_name: resp.user.first_name,
       last_name: resp.user.last_name,
       gender: resp.user.gender,
-      birthdate: resp.user.birthdate,
+      birthdate: formatbirthdate.toLocaleDateString("en-GB"),
       cf: resp.user.codice_fiscale,
       is_admin: resp.user.is_admin,
     })
   }
-    render(){
-      var options =[
-        {
-          header:{
-            name: "Account",
-          },
-      
-          values: [
-            {
-              name: "Name",
-              description: "Your personal data",
-              data: this.state.first_name,
-            },
-            {
-              name: "Surname",
-              description: "Your personal data",
-              data: this.state.last_name,
-            },
-            {
-              name: "Gender",
-              description: "m/f",
-              data: this.state.gender,
-            },
-            {
-              name: "Birthdate",
-              description: "Your bithdate",
-              data: this.state.birthdate,
-            },
-            {
-              name: "Tax number",
-              description: "Your tax number",
-              data: this.state.cf,
-            },
-          ],
+  render() {
+    var options = [
+      {
+        header: {
+          name: "Account",
         },
-      
-        {
-          header:{
-            name: "Update",
+
+        values: [
+          {
+            name: "Name",
+            description: "Your personal data",
+            data: this.state.first_name,
           },
-      
-          values: [
-            {
-              name: "Are you pro?",
-              description: "Your current account",
-            },
-            {
-              name: "Billing Information",
-              description: "Manage your billing information",
-            },
-            {
-              name: "Invoices",
-              description: "Track your invoices and their status",
-            },
-          ],
-        },
-      
-        {
-          header:{
-            name: "Support",
+          {
+            name: "Surname",
+            description: "Your personal data",
+            data: this.state.last_name,
           },
-      
-          values: [
-            {
-              name: "Help",
-              description: "Having trouble",
-            },
-            {
-              name: "FAQ",
-              description: "View our frequently asked questions",
-            },
-          ],
+          {
+            name: "Gender",
+            description: "m/f",
+            data: this.state.gender,
+          },
+          {
+            name: "Birthdate",
+            description: "Your bithdate",
+            data: this.state.birthdate,
+          },
+          {
+            name: "Tax number",
+            description: "Your tax number",
+            data: this.state.cf,
+          },
+        ],
+      },
+
+      {
+        header: {
+          name: "Update",
         },
-      ];
+
+        values: [
+          {
+            name: "Are you pro?",
+            description: "Your current account",
+          },
+          {
+            name: "Billing Information",
+            description: "Manage your billing information",
+          },
+          {
+            name: "Invoices",
+            description: "Track your invoices and their status",
+          },
+        ],
+      },
+
+      {
+        header: {
+          name: "Support",
+        },
+
+        values: [
+          {
+            name: "Help",
+            description: "Having trouble",
+          },
+          {
+            name: "FAQ",
+            description: "View our frequently asked questions",
+          },
+        ],
+      },
+    ];
 
 
-        return (
-          
-        <div>
-            <button onClick={this.props.apriMenu} className='menu'><i className='fas fa-bars'/></button>
-            <h1>Settings</h1>
-            <div className="home">
-              
-              <div className='homeContainer'>
-                
-              {options.map((option)=> (
+    return (
 
-                <div key= {option.header.name} className="group">
-                  <h3> {option.header.name}</h3>
+      <div>
+        <button onClick={this.props.apriMenu} className='menu'><i className='fas fa-bars' /></button>
+        <h1>Settings</h1>
+        <div className="home">
 
-                  <ListGroup as="ul">
-                  {option.values.map((value)=>(
-                  <div key= {value.name}>
-                    <ListGroup.Item as="li"className="d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                    <div className="fw-bold">
-                      {value.name}
+          <div className='homeContainer'>
+
+            {options.map((option) => (
+
+              <div key={option.header.name} className="group">
+                <h3> {option.header.name}</h3>
+
+                <ListGroup as="ul">
+                  {option.values.map((value) => (
+                    <div key={value.name}>
+                      <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                          <div className="fw-bold">
+                            {value.name}
+                          </div>
+                          {value.description}
+                          <div>
+                            {value.data}
+
+                          </div>
+
+                        </div>
+
+                      </ListGroup.Item>
+
                     </div>
-                    {value.description}
-                    <div>
-                    {value.data}
-
-                    </div>
-                    
-                  </div>
-                
-                  </ListGroup.Item>
-                  
-                  </div>
                   ))}
-                  </ListGroup>
-          
-                  </div>  
-                  
-              ))}
-            </div>
-            
-            </div>
-        </div>);
-    }
+                </ListGroup>
+
+              </div>
+
+            ))}
+          </div>
+
+        </div>
+      </div>);
+  }
 }
 
 export default SettingsPage;
