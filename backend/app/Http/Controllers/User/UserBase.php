219d3bd1a16310ce9bad9baec99c0265bc9e6860
Andrea Rota <andrea.rota.98@gmail.com>
2023-02-07 16:46:43 +0100

    Prelievo fondi e relativi test

--- backend/app/Http/Controllers/User/UserBase.php
+++ backend/app/Http/Controllers/User/UserBase.php
@@ -14,7 +14,7 @@ class UserBase extends UserAbstract
      * @var App\Models\User
      */
     private $user;
-    
+
     /**
      * We establish this function in our controller class so that we can use the auth:api middleware
      * within it to block unauthenticated access to certain methods within the controller
@@ -67,7 +67,46 @@ class UserBase extends UserAbstract
             throw new AuthorizationException('non autorizzato');
         }
 
-        $this->user->wallet += $request->float('increase');
+        $amount = $request->float('increase');
+
+        // if deposit 
+        if ($amount >= 0) {
+            $this->user->wallet += $amount;
+            $this->user->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Wallet updated successfully',
+                'user' => $this->user,
+            ]);
+        }
+
+        // if withdraw
+        if ($this->user->wallet >= $amount) {
+
+            /**
+             * Qua ci va la logica di collegamento con l'istituto bancario
+             */
+
+            $this->user->wallet += $amount;
+        } else {
+            $totalStocksValue = 0;
+
+            $this->user->stocks->each(function ($item) use ($totalStocksValue) {
+                $totalStocksValue += ($item->number_stocks * $item->current_value);
+            });
+
+            if ($totalStocksValue >= $amount) {
+                $this->user->wallet += $amount;
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unavailable funds',
+                    'user' => $this->user,
+                ]);
+            }
+        }
+
         $this->user->save();
 
         return response()->json([
